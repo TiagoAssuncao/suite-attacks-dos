65cb30c849bbf800ece9e12b00522cded7056f30
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-09-14 20:59:46 -0300

    Waiting and kill child process
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- main.c
+++ main.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include<pthread.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
 
 int set_baseline(char* program, char** arg_list)
 {
     pid_t child_pid;
     child_pid = fork ();
-    if (child_pid != 0)
+    if (child_pid != 0){
+        sleep(5);
+        kill(child_pid, SIGTERM);
         return child_pid;
+    }
     else {
         execvp (program, arg_list);
         fprintf (stderr, "an error occurred in execvp\n");
