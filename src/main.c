8dbebc415365184afc7d25bc54178c55d9b3ab37
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-09-15 10:49:13 -0300

    Adding header to attack_process

--- src/main.c
+++ src/main.c
@@ -1,38 +1,8 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <unistd.h>
 #include <signal.h>
-
-#define PROGRAM_PATH 0
-#define IP 1
-#define RATE_FLAG 2
-#define RATE_NUMBER 3
-#define FINAL_ARG 4
-#define SIZE_PATH 1024
-
-int call_attack(char* program, char** arg_list, int time_to_live){
-    pid_t child_pid;
-    child_pid = fork ();
-    if (child_pid != 0){
-        sleep(time_to_live);
-        kill(child_pid, SIGTERM);
-        return child_pid;
-    }
-    else {
-        execvp (program, arg_list);
-        fprintf (stderr, "an error occurred in execvp\n");
-        abort ();
-    }
-}
-
-char* get_loic_path(char* cwd){
-    getcwd(cwd, SIZE_PATH * sizeof(char));
-    strcat(cwd, "/bin/loic");
-
-    return cwd;
-}
+#include "attack_process.h"
 
 int main(int argc, const char *argv[]){
     char* cwd = malloc(SIZE_PATH * sizeof(char));
