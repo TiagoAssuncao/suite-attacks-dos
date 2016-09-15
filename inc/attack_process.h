8e7e4b07018590773802d87661d77ca2d5c33a73
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-09-15 10:49:37 -0300

    Adding header files

---
+++ inc/attack_process.h
@@ -0,0 +1,21 @@
+#ifndef ATTACK_PROCESS_H
+#define ATTACK_PROCESS_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+extern int PROGRAM_PATH;
+extern int IP;
+extern int RATE_FLAG;
+extern int RATE_NUMBER;
+extern int FINAL_ARG;
+extern int SIZE_PATH;
+
+int call_attack(char* program, char** arg_list, int time_to_live);
+
+char* get_loic_path(char* cwd);
+
+#endif
