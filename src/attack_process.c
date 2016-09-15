913aa8765b0f1386237636098205e0024cb9564a
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-09-15 17:18:38 -0300

    Comment and putting const on process .c
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- src/attack_process.c
+++ src/attack_process.c
@@ -7,24 +7,44 @@ int RATE_NUMBER = 3;
 int FINAL_ARG = 4;
 int SIZE_PATH = 1024;
 
-int call_attack(char* program, char** arg_list, int time_to_live){
+/* Create a child process and the parent will wait time_to_live seconds */
+/* to send the SIGTERM signal to kill him */
+int call_attack(
+        const unsigned char* program,
+        char** arg_list,
+        const unsigned int time_to_live
+        ){
     pid_t child_pid;
-    child_pid = fork ();
+
+    /* Create a new process */
+    child_pid = fork();
+
+    /* If it is the parent process, wait a time_to_live */
+    /* and kill the child */
     if (child_pid != 0){
         sleep(time_to_live);
         kill(child_pid, SIGTERM);
         return child_pid;
     }
     else {
+        /* Execute the child process (loic) */
         execvp (program, arg_list);
         fprintf (stderr, "an error occurred in execvp\n");
         abort ();
     }
 }
 
-char* get_loic_path(char* cwd){
+/* Get path of loic in the system */
+char* get_loic_path(){
+    char* cwd;
+    cwd = malloc(SIZE_PATH * sizeof(char));
     getcwd(cwd, SIZE_PATH * sizeof(char));
     strcat(cwd, "/bin/loic");
 
     return cwd;
+}
+
+char** alloc_arg_list(){
+    char** arg_list = malloc(5 * sizeof(char*));
+    return arg_list;
 }
